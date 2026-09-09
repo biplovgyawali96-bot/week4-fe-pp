eec95cacce6da45b40424016555c040c18969e11
Biplov Gyawali <biplovg@metropolia.fi>
2026-09-09 14:55:56 +0300

    feat: add removal functionality to Tours

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,18 +1,41 @@
+
+import { useState } from "react";
 import { tours } from "../data";
+
 import Title from "./Title";
 import Tour from "./Tour";
 
 const Tours = () => {
+  const [toursData, setToursData] = useState(tours);
+
+  const removeTour = (id) => {
+    setToursData(
+      toursData.filter((tour) => tour.id !== id)
+    );
+  };
+
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
-        {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
+        {toursData.map((tour) => {
+          return (
+            <div key={tour.id}>
+              <Tour {...tour} />
+
+              <button
+                className="remove-btn"
+                onClick={() => removeTour(tour.id)}
+              >
+                Remove
+              </button>
+            </div>
+          );
         })}
       </div>
     </section>
   );
 };
+
 export default Tours;
