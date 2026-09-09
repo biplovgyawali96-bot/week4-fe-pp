e5995fe5b8b58a622aaf57a350e3da90d4fd9c3e
Pratham Arora <prathamarora9f15@gmail.com>
2026-09-09 14:20:14 +0300

    refactor: convert Services to use local state

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,22 @@
-import { services } from '../data'
-import Title from './Title'
-import Service from './Service'
+import { useState } from "react";
+import { services } from "../data";
+import Title from "./Title";
+import Service from "./Service";
 
 const Services = () => {
-  return (
-    <section className='section services' id='services'>
-      <Title title='our' subTitle='services' />
+  const [servicesData, setServicesData] = useState(services);
 
-      <div className='section-center services-center'>
-        {services.map((service) => {
-          return <Service {...service} key={service.id} />
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subTitle="services" />
+
+      <div className="section-center services-center">
+        {servicesData.map((service) => {
+          return <Service {...service} key={service.id} />;
         })}
       </div>
     </section>
-  )
-}
-export default Services
+  );
+};
+
+export default Services;
