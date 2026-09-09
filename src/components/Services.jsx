cd377a114a98f35b461010aec7905f33f1998b34
Biplov Gyawali <biplovg@metropolia.fi>
2026-09-09 14:31:55 +0300

    feat: add removal functionality to Services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,41 @@
+
+import { useState } from 'react'
 import { services } from '../data'
+
 import Title from './Title'
 import Service from './Service'
 
 const Services = () => {
+  const [servicesData, setServicesData] = useState(services)
+
+  const removeService = (id) => {
+    setServicesData(
+      servicesData.filter((service) => service.id !== id)
+    )
+  }
+
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
 
       <div className='section-center services-center'>
-        {services.map((service) => {
-          return <Service {...service} key={service.id} />
+        {servicesData.map((service) => {
+          return (
+            <div key={service.id}>
+              <Service {...service} />
+
+              <button
+                className='remove-btn'
+                onClick={() => removeService(service.id)}
+              >
+                Remove
+              </button>
+            </div>
+          )
         })}
       </div>
     </section>
   )
 }
+
 export default Services
